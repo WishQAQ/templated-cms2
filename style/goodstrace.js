243157de59db48a4bfb6005e7477e53178220e76
Wish_Time <wish-time@outlook.com>
2017-12-08 10:44:31 +0800

    Initial commit

---
+++ style/goodstrace.js
@@ -0,0 +1,175 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	$("#indexQueryBill").click(function(){
+		var waybill = $(this).prev().val();
+		var reg = /^([a-zA-Z0-9]|\d{2})\d{7}$/;
+		if(!reg.test(waybill)){
+			errTips("运单号格式有误");
+			return;
+		}
+		window.location.href = "traceTransByNo.action?transNos="+waybill;
+	});
+	
+	/**网点查询**/
+	$("#queryDistrictBtn").click(function(){
+		var $typeValue = $("#typeValue");
+		if($typeValue.attr("ov") == $typeValue.val()){
+			$typeValue.attr("style","border:1px solid #f15a22");
+			return;
+		}
+		$("#districtForm").submit();
+	});
+	
+	var flag = true;
+	// 验证码输入框失去焦点 ajax 异步验证
+	$("#validateCode_input").blur(function(){
+		var validateCode =  $(this).val();
+		if(validateCode.length > 0){
+			$.get("codeCheck.action?code="+validateCode,function(data,status){
+				if(data.success == false){
+					$("#codeNotice").text(data.message).show();
+					flag = false;
+				}else{
+					flag = true;
+				}
+			});
+		}
+	});
+	
+	// 验证码输入框失去焦点
+	$("#validateCode_input").focus(function(){
+		$("#codeNotice").hide();
+	});
+	
+	// 查询货物跟踪信息
+	$("#wayBillQuery_btn").click(function(){
+		var waybills = $("#waybillVale_input").val();
+		waybills = $.trim(waybills);
+		var validateCode = $("#validateCode_input").val();
+		
+		waybills=waybills.replace(/\n/g, '_@').replace(/\r/g, '_#');		
+		waybills = waybills.replace(/_#_@/g, '<br/>').replace(/_@/g, '<br/>');
+
+		if(flag == false){
+			$("#codeNotice").text("验证码输入有误").show()
+			return;
+		}
+		
+		if(waybills == "" || validateCode == ""){
+			$("#codeNotice").text("运单号或者验证码不能为空").show()
+			return;
+		}
+		
+		var validValue = waybills.replace(new RegExp(/(\<br\/\>)/g),',');
+		var validArr = validValue.split(',');
+		if(validArr.length>10){
+			$("#isOverMaxlength").show();
+			return;
+		}else{
+			$("#isOverMaxlength").hide();
+		}
+		
+		
+		for(var i = 0; i < validArr.length; i++) {
+			if($.trim(validArr[i])==""){
+				$("#codeNotice").text("逗号或者换行直接不能出现空单号").show();
+			}else if(!isWaybill(validArr[i])){
+				$("#codeNotice").text(validArr[i]+"不是正确的单号").show();
+				return;
+			} 
+		} 
+		
+		
+		// cookie操作
+		var w = getCookie('importList');
+		var cookieValue;
+		if(w == null || w == ''){
+			cookieValue = waybills;
+		}else{
+			cookieValue = waybills + ',' + w;
+		}
+		cookieValue=cookieValue.replace(new RegExp(/(\<br\/\>)/g),',');
+		var arr = cookieValue.split(',');
+		// 去重复
+		arr.sort();
+		var re=[arr[0]];
+		for(var i = 1; i < arr.length; i++)
+		{
+			if( arr[i] !== re[re.length-1])
+			{
+				if($.trim(arr[i])!=""){
+					re.push(arr[i]);
+				}
+			}
+		}
+		
+
+		// 过期时间 30天
+		setCookie('importList',re.join(','),30);
+		window.location.href = "traceTransByNo.action?vercode="+validateCode+"&transNos="+waybills;
+		
+	});
+	
+	
+	// 清空查询记录
+	$("#clearQueryRecord").click(function(){
+		$("#importList").text('');
+		delCookie('importList');
+	});
+	
+	// 清空查询记录
+	$("#send_email_link").click(function(){
+		var emailAddress = $("#emails_input").val();
+		
+		var waybills = $("#waybillVale_input").val();
+		if(waybills == null || waybills.length == 0){
+			alert("运单号不能为空");
+			return;
+		}
+		
+		if(isEmail(emailAddress) == false){
+			alert("请输入正确的邮箱地址");
+			return;
+		}
+		
+		$.get("sendEmail.action?emailAddress="+emailAddress,function(data,status){
+			if(data.success == true){
+				alert("已发送，请查收");
+			}
+		});
+	});
+	
+});
+
+// 更改验证码
+function changeCode(){  
+	$("#validateCode_img").attr("src","genCheckCode.action?"+ new Date());
+} 
+
+function errTips(msg){
+	$(".erro").show();
+	$(".erro_msg").text(msg)
+}
+
+function initval(value){
+	if(value==1){
+		if($("#waybillVale_input").val()=="请输入单号,最多可同时查询10个运单,多单号间用回车或逗号隔开"){
+			$("#waybillVale_input").val("");
+		}
+	}else{
+		if(isEmpty($("#waybillVale_input").val())){
+			$("#waybillVale_input").val("请输入单号,最多可同时查询10个运单,多单号间用回车或逗号隔开");
+		}
+	}
+}
+
+
+function isEmpty(str){
+	if($.trim(str) == '' || str == null){
+		return true;
+	}
+	return false;
+}
+
---
+++ style/queryPriceTime.js
@@ -0,0 +1,342 @@
+var setLeavedCityName = "出发城市";
+var setArrivedCityName = "到达城市";
+var setWeight = "货物总重量";
+var setVolumn = "货物总体积:长*宽*高(CM)";
+var setInsurance = "请输入货物实际价值进行保价";
+var setCollDeliveryAmount = "如您需要代收货款服务则填写";
+
+$(document).ready(function(){
+	
+	$("#queryPricTime").click(function(){
+		var leavedPriceTimeCityName = $("#leavedCityName").val(); 								
+		var arrivedPriceTimeCityName = $("#arrivedCityName").val(); 
+		var reg = /^[\u4E00-\u9FA5]{2,}-[\u4E00-\u9FA5]{2,}-[\u4E00-\u9FA5]{2,}$/;
+		if(!reg.test(leavedPriceTimeCityName) && !reg.test(arrivedPriceTimeCityName)){
+			return ;
+		}else{
+			$("#priceTimeForm").submit();
+		}
+	});
+	
+	$(function(){
+		var leavedCityName = $("#leavedCityName").val();
+		var arrivedCityName = $("#arrivedCityName").val();
+		var reg = /^[\u4E00-\u9FA5]{2,}-[\u4E00-\u9FA5]{2,}-[\u4E00-\u9FA5]{2,}$/;
+		if(leavedCityName != null && reg.test(leavedCityName) && arrivedCityName != null && reg.test(arrivedCityName)){
+			showPriceTime();
+		}
+	});
+	
+	/**
+	 * 查询价格时效
+	 */
+	$("#queryPrice").click(function (){
+		showPriceTime();
+	});
+	
+	/**
+	 * 初始化禁用计算button
+	 */
+	/*$(function(){
+		$("#priceCale").attr({"disabled":"disabled"});
+	});*/
+	
+	/**
+	 * 计算价格
+	 */
+	$("#priceCale").click(function (){
+		var leavedCityName = $("#leavedCityName").val();
+		//没有选择出发城市
+		if(setLeavedCityName == leavedCityName){
+			$("#routeMsg").removeClass("hide");
+			return;
+		}
+		var shippers = leavedCityName.split("-");
+		var shipperCity = shippers[1];
+		var shipperCounty = shippers[2];
+		
+		var arrivedCityName = $("#arrivedCityName").val();
+		//没有选择出发城市
+		if(setArrivedCityName == arrivedCityName){
+			$("#routeMsg").removeClass("hide");
+			return;
+		}
+		var con = arrivedCityName.split("-");
+		var conCity = con[1];
+		var conCounty = con[2];
+		
+		//二次选择正确路线
+		$("#routeMsg").addClass("hide");
+		
+		//重量
+		var weight = $("#weight").val();
+		if (setWeight == weight || weight == "") {
+			$("#tips").text("请输入货物重量!");
+			return;
+		}else if(weight == 0){
+			$("#tips").text("货物重量不能为零!");
+			return;
+		}else if(isNaN(weight)){
+			$("#tips").text("请输入正确的货物重量!");
+			return;
+		}
+		//体积
+		var volumn = $("#volumn").val();
+		if (setVolumn == volumn || volumn == "") {
+			$("#tips").text("请输入货物体积!");
+			return;
+		}else if(volumn == 0){
+			$("#tips").text("货物体积不能为零!");
+			return ;
+		}else if(isNaN(volumn)){
+			$("#tips").text("请输入正确的货物体积!");
+			return ;
+		}
+		//保价
+		var insurance = $("#insurance").val();
+		if (setInsurance == insurance || insurance == "") {
+			$("#tips").text("请输入货物保价!");
+			return;
+		}else if(isNaN(insurance)){
+			$("#tips").text("请输入正确的保价金额!");
+			return;
+		}
+		$("#tips").text("");
+		
+		//代收货款
+		var collDeliveryAmount = $("#collDeliveryAmount").val();
+		if(setCollDeliveryAmount == collDeliveryAmount || collDeliveryAmount == ""){
+			collDeliveryAmount = 0;
+		}
+		
+		
+		$.ajax({
+			type : "post",
+			url : "priceCalc.action",
+			dataType : "json",
+			contentType: "application/x-www-form-urlencoded; charset=utf-8", 
+			data : {
+				"priceQueryVo.conCity":conCity,
+				"priceQueryVo.conCounty":conCounty,
+				"priceQueryVo.shipperCity":shipperCity,
+				"priceQueryVo.shipperCounty":shipperCounty,
+//				"priceQueryVo.productTypeId":productTypeId,
+				"priceQueryVo.weight":weight,
+				"priceQueryVo.volumn":volumn,
+				"priceQueryVo.insurance":insurance,
+				"priceQueryVo.collDeliveryAmount":collDeliveryAmount
+			},
+			success : function(data) {
+				if (data.success) {
+					var prices = data.priceCalcVos;
+					var tail = 0;
+					for(var i=0;i<prices.length;i++){
+						if(prices[i].transType == "ONTIME"){
+							tail = 0;
+						}else if(prices[i].transType == "LESSLOADED"){
+							tail = 1;
+						}
+						$("#goodsType"+tail).text(prices[i].goodsType);
+						$("#transAging"+tail).text(prices[i].transAging);
+						$("#transCost"+tail).text(prices[i].transCost+" 元");
+						$("#insuredCost"+tail).text(prices[i].insuredCost+" 元");
+						$("#fuelCost"+tail).text(prices[i].fuelCost+" 元");
+						$("#laborCost"+tail).text(prices[i].laborCost+" 元");
+						$("#messageCost"+tail).text(prices[i].messageCost+" 元");
+						$("#collProceCost"+tail).text(prices[i].collProceCost+" 元");
+						$("#totalCost"+tail).text(prices[i].totalCost+" 元");
+						
+					}
+					$("#price_cale").removeClass("hide");
+					$("#explain_div").removeClass("hide");
+				} else {
+					alert(data.message);
+				}
+			},
+			error : function(data) {
+				alert("对不起，系统繁忙,请稍后操作！");
+			}
+		});
+	});
+	
+	
+});
+
+/**
+ * 
+ * @author 龙海仁
+ * @date 2015年8月21日下午2:07:52
+ * @update
+ */
+function showPriceTime(){
+	var leavedCityName = $("#leavedCityName").val();
+	//没有选择出发城市
+	if(setLeavedCityName == leavedCityName){
+		$("#routeMsg").removeClass("hide");
+		return;
+	}
+	//匹配格式
+	var reg = /^[\u4E00-\u9FA5]{2,}-[\u4E00-\u9FA5]{2,}-[\u4E00-\u9FA5]{2,}$/;
+	if(!reg.test(leavedCityName)){
+		$("#routeMsg").removeClass("hide");
+		return;
+	}
+	var shippers = leavedCityName.split("-");
+	var shipperCity = shippers[1];
+	var shipperCounty = shippers[2];
+	
+	//到达城市
+	var arrivedCityName = $("#arrivedCityName").val();
+	//没有选择出发城市
+	if(setArrivedCityName == arrivedCityName){
+		$("#routeMsg").removeClass("hide");
+		return;
+	}
+	if(!reg.test(arrivedCityName)){
+		$("#routeMsg").removeClass("hide");
+		return;
+	}
+	var con = arrivedCityName.split("-");
+	var conCity = con[1];
+	var conCounty = con[2];
+	
+	//二次选择正确路线
+	$("#routeMsg").addClass("hide");
+	
+	$.ajax({
+		type : "post",
+		url : "queryPriceTime.action",
+		dataType : "json",
+		contentType: "application/x-www-form-urlencoded; charset=utf-8", 
+		data : {
+			"priceQueryVo.conCity":conCity,
+			"priceQueryVo.conCounty":conCounty,
+			"priceQueryVo.shipperCity":shipperCity,
+			"priceQueryVo.shipperCounty":shipperCounty,
+//			"priceQueryVo.productTypeId":productTypeId,
+//			"priceQueryVo.weight":weight,
+//			"priceQueryVo.volumn":volu/*
+		},
+		success : function(data) {
+			if (data.success) {
+				var prices = data.priceTimeVos;
+				var tail = 0;
+				for(var i=0;i<prices.length;i++){
+					if(prices[i].transportType == "ONTIME"){
+						tail = 0;
+					}else if(prices[i].transportType == "LESSLOADED"){
+						tail = 1;
+					}
+					$("#pickup"+tail).text("第"+prices[i].pickTime+"天");
+					$("#delivery"+tail).text("第"+prices[i].deliveryTime+"天");
+					$("#startPrice"+tail).text(prices[i].startPrice);
+					$("#heavyPrice"+tail).text(prices[i].heavyPrice);
+					$("#lightPrice"+tail).text(prices[i].lightPrice);
+					
+					$("#price"+tail).removeClass("hide");
+				}
+				$("#price_time").removeClass("hide");
+				$("#price_cale").removeClass("show").addClass("hide");
+				$("#trans_illustrate").removeClass("show").addClass("hide");
+			} else {
+				alert(data.message);
+			}
+		},
+		serror : function(data) {
+			alert("对不起，系统繁忙,请稍后操作！");
+		}
+	});
+}
+
+/**
+ * 输入框验证
+ * @param id
+ * @param operation
+ * @param whichOne
+ */
+function initval(id, operation, whichOne) {
+	var vs = $("#"+id).val();
+	var setv;
+	switch(whichOne){
+	case 0:
+		setv = setLeavedCityName;
+		break;
+	case 1:
+		setv = setArrivedCityName;
+		break;
+	case 2:
+		if(operation == 2){
+			validateParam() ;
+		}
+		setv = setWeight;
+		break;
+	case 3:
+		if(operation == 2){
+			//体积
+			validateParam() ;
+		}
+		setv = setVolumn;
+		break;
+	case 4:
+		if(operation == 2){
+			//保价
+			validateParam() ;
+		}
+		setv = setInsurance;
+		break;
+	case 5:
+		setv = setCollDeliveryAmount;
+		break;
+	}
+	if(operation==1){
+		if(vs==setv){
+			$('#'+id).val("");
+			$('#'+id).removeClass("grays");
+		}
+	}
+	if(operation==2){
+		if(vs.length==0){
+			$('#'+id).val(setv);
+			$('#'+id).addClass("grays");
+		}
+	}
+}
+
+function validateParam() {
+	//重量
+	var weight = $("#weight").val();
+	if (setWeight == weight || weight == "") {
+		$("#tips").text("请输入货物重量!");
+		return;
+	}else if(weight == 0){
+		$("#tips").text("货物重量不能为零!");
+		return;
+	}else if(isNaN(weight)){
+		$("#tips").text("请输入正确的货物重量!");
+		return;
+	}
+	//体积
+	var volumn = $("#volumn").val();
+	if (setVolumn == volumn || volumn == "") {
+		$("#tips").text("请输入货物体积!");
+		return;
+	}else if(volumn == 0){
+		$("#tips").text("货物体积不能为零!");
+		return ;
+	}else if(isNaN(volumn)){
+		$("#tips").text("请输入正确的货物体积!");
+		return ;
+	}
+	//保价
+	var insurance = $("#insurance").val();
+	if (setInsurance == insurance || insurance == "") {
+		$("#tips").text("请输入货物保价!");
+		return;
+	}else if(isNaN(insurance)){
+		$("#tips").text("请输入正确的保价金额!");
+		return;
+	}
+	
+	$("#tips").text("");
+	 
+}
